5c3848db0d8c524de06e0b7671609e8363d4d12c
achillesbissias <138939321+achillesbissias@users.noreply.github.com>
2023-09-07 16:22:33 -0500

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,1 +1,14 @@
+# include <iostream>
+using namespace std;
 
+int main() {
+
+    string name;
+
+    cout << "What is your name?" << endl;
+    cin >> name;
+
+    cout << "Hello, " << name << "!" << endl;
+
+    return 0;
+}
